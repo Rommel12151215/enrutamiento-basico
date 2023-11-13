e4fe93e43c87173655ebc0aa1a02cb0ab727b65e
Rommel <karlsrommel255@gmail.com>
2023-11-12 22:06:09 -0600

    EnrutamientoBasico

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '/views/';
+switch ($request) {
+   case '':
+   case '/':
+       require __DIR__ . $viewDir . 'home.php';
+       break;
+   case '/contact':
+       require __DIR__ . $viewDir . 'contact.php';
+       break;
+   default:
+       http_response_code(404);
+       require __DIR__ . $viewDir . '404.php';
+}
